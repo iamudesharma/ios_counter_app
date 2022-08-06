4e1720c8d363d05665943d8a1126f1aaab5bdecb
iamudesharma <udesh2568@gmail.com>
2022-08-06 20:28:13 +0530

    some fix added

--- CounterApp/ContentView.swift
+++ CounterApp/ContentView.swift
@@ -40,6 +40,46 @@ struct ContentView: View {
                             radius: 3,
                             x: 3,
                             y: 3)
+                    
+                    Button(action: {
+                        
+                        
+                        self.items-=1;
+        //                self.items.append(Item(value: "Item"))
+                    }, label: {
+                        Text("-")
+                            .font(.system(.largeTitle))
+                            .frame(width: 77, height: 70)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 7)
+                    })
+                    .background(Color.indigo)
+                    .cornerRadius(38.5)
+                    .padding()
+                    .shadow(color: Color.black.opacity(0.3),
+                            radius: 3,
+                            x: 3,
+                            y: 3)
+                    
+                    Button(action: {
+                        self.items=0;
+        //                self.items.append(Item(value: "Item"))
+                    }, label: {
+                        Text("0")
+                            .font(.system(.largeTitle))
+                            .frame(width: 77, height: 70)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 7)
+                    })
+                    .background(Color.red)
+                    .cornerRadius(38.5)
+                    .padding()
+                    .shadow(color: Color.black.opacity(0.3),
+                            radius: 3,
+                            x: 3,
+                            y: 3)
+
+
                 }
             }
         }
