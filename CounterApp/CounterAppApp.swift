dcb24d30c07df9efd1b92fb7285741150740f495
iamudesharma <udesh2568@gmail.com>
2022-08-06 20:03:00 +0530

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by udesh sharma on 06/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
