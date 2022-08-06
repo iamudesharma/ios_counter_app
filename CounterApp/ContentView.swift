1640d0fc0c6eccf0488705e08e8de15a5513c482
iamudesharma <udesh2568@gmail.com>
2022-08-06 20:22:18 +0530

    first commits

--- CounterApp/ContentView.swift
+++ CounterApp/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var  items : Int32 = 0;
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            VStack(){
+               
+                
+                
+                Text("Current count \(items)")
+                    .padding()
+                
+                Spacer()
+                
+                HStack(){
+                    Spacer()
+                    Button(action: {
+                        self.items+=1;
+        //                self.items.append(Item(value: "Item"))
+                    }, label: {
+                        Text("+")
+                            .font(.system(.largeTitle))
+                            .frame(width: 77, height: 70)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 7)
+                    })
+                    .background(Color.blue)
+                    .cornerRadius(38.5)
+                    .padding()
+                    .shadow(color: Color.black.opacity(0.3),
+                            radius: 3,
+                            x: 3,
+                            y: 3)
+                }
+            }
+        }
+        
     }
 }
 
